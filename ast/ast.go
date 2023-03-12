381d7df5bfeaf535106b2dbb434a21d0a494763c
siddhantdubey <sid12.dubey@gmail.com>
2023-03-12 18:34:27 -0400

    added some parser stuff

--- ast/ast.go
+++ ast/ast.go
@@ -1,46 +1,56 @@
 package ast
+
 import "monkey/token"
 
 type Node interface {
-    TokenLiteral() string
+	TokenLiteral() string
 }
 
 type Statement interface {
-    Node
-    statementNode()
+	Node
+	statementNode()
 }
 
 type Expression interface {
-    Node
-    expressionNode()
+	Node
+	expressionNode()
 }
 
 type Program struct {
-    Statements []Statement
+	Statements []Statement
+}
+
+type ReturnStatement struct {
+    Token token.Token
+    ReturnValue Expression
 }
 
 func (p *Program) TokenLiteral() string {
-    if len(p.Statements) > 0 {
-        return p.Statements[0].TokenLiteral()
-    } else {
-        return ""
-    }
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
 }
-
 
 type LetStatement struct {
-    Token token.Token // the token.LET Token
-    Name *Identifier
-    Value Expression
+	Token token.Token // the token.LET Token
+	Name  *Identifier
+	Value Expression
 }
 
-func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Identifier struct {
-    Token token.Token // the token.IDENT token 
-    Value string
+	Token token.Token // the token.IDENT token
+	Value string
 }
 
-func (i *Identifier) expressionNode() {}
-func (i *Identifier) Tokenliteral() string {return i.Token.Literal}
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+    return rs.Token.Literal
+}
